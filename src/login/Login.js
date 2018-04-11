e846cc5fa2204b12144296f714ba431b342bc498
thiagolucio <thiagolucio@gmail.com>
2018-04-11 19:21:19 -0300

    add login logical prog

--- src/login/Login.js
+++ src/login/Login.js
@@ -2,31 +2,45 @@ import React, { Component } from 'react';
 import TextField from 'material-ui/TextField';
 import { Card, CardActions, CardTitle, CardText } from 'material-ui/Card';
 import RaisedButton from 'material-ui/RaisedButton';
-import FontIcon from 'material-ui/FontIcon';
 import './login.css';
 import logo from '../images/Touch_ID-128.png';
+// import FontIcon from 'material-ui/FontIcon';
 // import logo from '../images/login128-1.png';
 
 
 export default class Login extends Component {
+
+    _sendForm(event) {
+        event.preventDefault();
+        console.log('Form is sent');
+        var _data = {
+        email: document.getElementById('email').value,
+        password: document.getElementById('password').value
+        };
+        console.log(_data) ;
+};
+
 
     render() {
         return (
             <div>               
                 <Card className="container-login">
                     <img src={logo} alt="logo login" />
-                    <form>
+                    <form method="POST" onSubmit={(event) => this._sendForm(event)}>
                         <CardTitle title="LOGIN" subtitle="Please Enter your account to login" />
                         <CardText className="body-CardText">
                             <TextField
+                                id="email"    
                                 fullWidth={true}    
                                 className="textFields"
                                 hintText="your email ?"
                                 errorText="This field is required"
                                 floatingLabelText="Email"
+                                type="email"
                             />
                             <br/>
                             <TextField
+                                id="password"    
                                 fullWidth={true}    
                                 className="textFields"
                                 hintText="Your Password ?"
@@ -39,7 +53,7 @@ export default class Login extends Component {
                             </p>
                         </CardText>
                         <CardActions>
-                            <RaisedButton label="ENTER" fullWidth={true} primary={true} className="loginButton" labelStyle={{ fontSize: '18px' }}/>
+                            <RaisedButton type="submit" label="ENTER" fullWidth={true} primary={true} className="loginButton" labelStyle={{ fontSize: '18px' }}/>
                         </CardActions>
                     </form>
                 </Card>                  
