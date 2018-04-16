3aec47cb368880339fd66ca8441baa693475a144
thiagolucio <thiagolucio@gmail.com>
2018-04-16 18:08:22 -0300

    codigo comentado pelo alair

--- src/UsersList/UserListAlair.js
+++ src/UsersList/UserListAlair.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import './UsersList.css';
 import Avatar from 'material-ui/Avatar';
 import FontIcon from 'material-ui/FontIcon';
@@ -28,37 +28,50 @@ const colorDialogStyles = {
 
 export default class UsersList extends Component {
 
-    constructor() {
-        super();
-        this.state = { users: [] };
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            users: [],
+            open: false,
+            selectedUser: {}
+        };
+
+        this.openDetails = this.openDetails.bind(this);
     }
-    stateModal = {
-        open: false,
-    };
 
     componentDidMount() {
-        fetch('http://localhost:3004/user')
+        fetch('http://localhost:3004/users')
             .then(response => response.json())
-            .then(user => {
-                this.setState({ users: user });
-                console.log(user);
-                console.log(user[0].name);
+            .then(users => {
+                this.setState({ users });
             });
     }
 
+    // componentDidMount() {
+    //     const db = require('../db.json');
+
+    //     this.setState({ users: db.users });
+    // }
+
+    openDetails = (user) => {
+        this.setState({ open: true, selectedUser: user });
+    }
+
     render() {
-        if(!this.state.users) return <p>Loading...</p>
+        const { users, selectedUser } = this.state;
+
         return (
             <div>
                 <Paper zDepth={2}>
                     <Toolbar style={{ background: '#E0E0E0' }}>
-                    <ToolbarGroup firstChild={true}>
-                        <img src={barLogo} alt="logo login" className="barLogo" />
-                        <ToolbarTitle text="User List Control" />
-                    </ToolbarGroup>
-                    <ToolbarGroup>
-                        <RaisedButton label="Logout" primary={true} />
-                    </ToolbarGroup>
+                        <ToolbarGroup firstChild={true}>
+                            <img src={barLogo} alt="logo login" className="barLogo" />
+                            <ToolbarTitle text="User List Control" />
+                        </ToolbarGroup>
+                        <ToolbarGroup>
+                            <RaisedButton label="Logout" primary={true} />
+                        </ToolbarGroup>
                     </Toolbar>
                 </Paper>
 
@@ -67,13 +80,13 @@ export default class UsersList extends Component {
                     <List>
                         <Subheader><h2>Users Saves List</h2></Subheader>
                         {
-                            this.state.users.map(function (user) {
+                            users.map(user => {
                                 return (
                                     <ListItem key={user.id}
                                         primaryText={user.name}
                                         leftAvatar={<Avatar src={logo} />}
                                         rightIcon={<FontIcon className="material-icons" style={iconStyles}>open_in_browser</FontIcon>}
-                                        onClick={() => this.stateModal({ open: true })}
+                                        onClick={() => this.openDetails(user)}
                                     />
                                 );
                             })
@@ -84,13 +97,14 @@ export default class UsersList extends Component {
                 {/* Dialog */}
                 <FullscreenDialog
                     open={this.state.open}
-                    appBarStyle={{ background: '#880E4F'}}
+                    appBarStyle={{ background: '#880E4F' }}
                     onRequestClose={() => this.setState({ open: false })}
                     title={'User Details'}
                     style={colorDialogStyles}
-                    actionButton={<FlatButton label='Close' onClick={() => this.setState({ open: false })}/>}
+                    actionButton={<FlatButton label='Close' onClick={() => this.setState({ open: false })} />}
                 >
                     {/* Content inside here */}
+                    {JSON.stringify(selectedUser)}
                 </FullscreenDialog>
             </div>
         );
